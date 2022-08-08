a1ad974969770fdbce64ba16fffcc1de229c10c0
Ja90n <81578391+Ja90n@users.noreply.github.com>
2022-08-08 14:59:44 +0200

    Update MinigameEngine.java

--- src/main/java/com/ja90n/minigameengine/MinigameEngine.java
+++ src/main/java/com/ja90n/minigameengine/MinigameEngine.java
@@ -10,20 +10,18 @@ import com.ja90n.minigameengine.managers.ArenaManager;
 import com.ja90n.minigameengine.managers.PartyManager;
 import com.ja90n.minigameengine.runnables.ReceiveVelocityMessageRunnable;
 import com.velocitypowered.api.command.CommandManager;
-import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 @Plugin(
         id = "minigame-engine",
         name = "Minigame Engine",
-        version = "1.0.0"
+        version = "0.1.0"
 )
 public class MinigameEngine {
 
@@ -33,7 +31,6 @@ public class MinigameEngine {
     private PartyManager partyManager;
     private ArenaManager arenaManager;
     private JedisPool pool;
-    private Jedis jedis;
     private ReceiveVelocityMessageRunnable receiveVelocityMessageRunnable;
 
     @Inject
@@ -43,27 +40,17 @@ public class MinigameEngine {
         commandManager = server.getCommandManager();
         partyManager = new PartyManager(this);
         arenaManager = new ArenaManager(this);
-        logger.info("Hello there, it's a test plugin I made!");
+        logger.info("Thank you for using my system! - Ja90n");
     }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        // Do some operation demanding access to the Velocity API here.
-        // For instance, we could register an event:
-        CommandMeta meta = commandManager.metaBuilder("test")
-                // Specify other aliases (optional)
-                .build();
-
         server.getEventManager().register(this, new PlayerDisconnect(this));
 
         JedisPoolConfig poolCfg = new JedisPoolConfig();
         poolCfg.setMaxTotal(3);
 
         pool = new JedisPool(poolCfg, "192.168.178.16", 6379, 500);
-
-        try (Jedis jedis1 = pool.getResource()) {
-            System.out.println(jedis1.get("spigot:gamestate:"));
-        }
 
         commandManager.register("party",new PartyCommand(this));
         commandManager.register("p",new PartyCommand(this));
@@ -85,9 +72,5 @@ public class MinigameEngine {
 
     public PartyManager getPartyManager() {
         return partyManager;
-    }
-
-    public JedisPool getPool() {
-        return pool;
     }
 }
